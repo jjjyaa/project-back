4b48e564f81fee2f3b22fc65b93b121a8e1bb5d3
jjjyaa <as960503@naver.com>
2025-04-11 16:31:35 +0900

    11일 히주 최종

--- hjtest/src/main/java/com/example/hjtest/Dto/comment/CommentListResponseDto.java
+++ hjtest/src/main/java/com/example/hjtest/Dto/comment/CommentListResponseDto.java
@@ -1,12 +1,9 @@
 package com.example.hjtest.Dto.comment;
 
-import com.example.hjtest.entity.Board;
 import com.example.hjtest.entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -16,13 +13,13 @@ public class CommentListResponseDto {
     private String content;
     private String createdDatetime;
     private int boardId;
-    private String email;
+    private String name;
 
     public CommentListResponseDto(Comment comment) {
         this.commentId = comment.getCommentId();
         this.content = comment.getContent();
         this.createdDatetime = comment.getCreatedDatetime();
         this.boardId = comment.getBoard().getBoardId();
-        this.email = comment.getMember().getEmail();
+        this.name = comment.getMember().getName();
     }
 }
